08538ceb85a352941b6bf979aad9d04a9912cea6
mbharti321 <mbharti321@gmail.com>
2021-12-09 14:21:45 +0530

    add documentation using documentation tool

--- calculator/addition.js
+++ calculator/addition.js
@@ -1,9 +1,17 @@
 module.exports = addition;
 
+
+/**
+ * Add two numbers
+ * 
+ * @param {number} num1 The first number
+ * @param {number} num2 The second number
+ * @returns {number}
+ */
 function addition(num1, num2){
     return num1 + num2;
 }
 
-function add(num1, num2){
+function addTemp(num1, num2){
     return num1 + num2;
 }
