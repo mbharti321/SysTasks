6ed33f14f72fe4f02a4a053fba8ba1f742d4b59e
mbharti321 <mbharti321@gmail.com>
2021-12-09 10:32:32 +0530

    add calculator

---
+++ calculator/app.js
@@ -0,0 +1,15 @@
+// console.log("hello world!")
+var num1 = 10;
+var num2 = 20;
+
+function addition(num1, num2){
+    return num1 + num2;
+}
+
+function multiplication(num1, num2){
+    return num1 * num2;
+}
+dxa
+
+console.log("Addition of two numbers: " + addition(num1, num2));
+console.log("Multiplication of two numbers: " + multiplication(num1, num2));
